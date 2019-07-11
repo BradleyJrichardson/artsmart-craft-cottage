bc27de222b553dbc39522ef0201017d0a4372b7f
ms9440 <m.soheilirad@gmail.com>
2019-07-11 10:34:22 +1000

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import { Switch, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import PageNotFound from './components/PageNotFound';
 import { Elements, StripeProvider } from "react-stripe-elements";
 import CheckoutForm from "./CheckoutForm";
 
 class App extends Component {
   render() {
     return (
-      <StripeProvider apiKey="pk_test_TlwArxmeZUHrZWhICbGMWxA100dG0fErh2">
-        <Elements>
-          <CheckoutForm />
-        </Elements>
-      </StripeProvider>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+         
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </React.Fragment>
+      
+        
+      // <StripeProvider apiKey="pk_test_TlwArxmeZUHrZWhICbGMWxA100dG0fErh2">
+      //   <Elements>
+      //     <CheckoutForm />
+      //   </Elements>
+      // </StripeProvider>
     );
   }
 }

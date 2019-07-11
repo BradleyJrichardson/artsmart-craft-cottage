ccafde3b06829303080c2c49c84f92bf1807c27d
Cooper Viktor <im.cooperviktor@gmail.com>
2019-07-11 20:12:44 +1000

    final commit for the night

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,33 +6,36 @@ import { IoIosCart } from "react-icons/io";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className='navContainer'>
-        <div className='brandContainer'>
-          <h1 className='brand'>Artsmart Craft Cottage</h1>
-          <h1 className='nameLibby'>Designs by Libby Richardson</h1>
-        </div>
-        <div className='navLinksContainer'>
-          <div>
-            <Link to="/">
-              <h1>Home</h1>
+      <nav>
+        <div className='navContainer'>
+          <div className='brandContainer'>
+            <h1 className='brand'>Artsmart Craft Cottage</h1>
+            <h1 className='nameLibby'>Designs by Libby Richardson</h1>
+          </div>
+          <div className='navLinksContainer'>
+            <div>
+              <Link to="/">
+                <h1>Home</h1>
+              </Link>
+            </div>
+            <div>
+              <Link to="/checkout">
+                <h1>Checkout</h1>
+              </Link>
+            </div>
+            <div>
+              <Link to="/products">
+                <span>All products</span>
+              </Link>
+            </div>
+          </div>
+          <div className='iconSpacing'>
+            <Link to='/checkout'>
+              < IoIosCart className='cartIcon'/>
             </Link>
           </div>
-          <div>
-            <Link to="/checkout">
-              <h1>Checkout</h1>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products">
-              <span>All products</span>
-            </Link>
-          </div>
         </div>
-        <div className='iconSpacing'>
-          <Link to='/checkout'>
-            < IoIosCart className='cartIcon'/>
-          </Link>
-        </div>
+        {/* <div className='spacingLine'></div> */}
       </nav>
     );
   }

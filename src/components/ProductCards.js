2187cbff0ed1f7f53d1fe251002726d35cc57c34
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-12 10:28:42 +1000

    some stuff

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProductCards = props => {
   let { title, description, categories } = props.product;
   return (
-    <div className="product-card">
+    <div className="card">
       <h1>{title}</h1>
       <p>{description}</p>
       <p>{categories[0]}</p>

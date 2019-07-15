deea7498953c70efd33ba13d9b76e700f22d19fa
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-15 14:03:52 +1000

    cart logic

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import ProductSection from "./components/ProductSection";
 import axios from "axios";
 import { Elements, StripeProvider } from "react-stripe-elements";
 import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
 
 export default class App extends React.Component {
   state = {
     products: null,
     cart: [],
-    cartSubTotal: 0,
-    cartTotal: 90
+    cartTotal: 90,
+    cartOpen: true
   };
 
   async componentDidMount() {
@@ -32,12 +33,42 @@ export default class App extends React.Component {
   }
 
   addToCart = product => {
+    let { product_id, sku, price, title } = product;
+
+    const productObj = {
+      title: title,
+      product_id: product_id,
+      sku: sku,
+      price: price
+    };
     this.setState(() => {
       return {
-        cart: [...this.state.cart, product]
+        cart: [...this.state.cart, productObj]
+      };
+    });
+    this.calcTotal();
+  };
+
+  calcTotal = () => {
+    let prices = this.state.cart.map(productObj => {
+      return productObj.price;
+    });
+    let total = prices.reduce((a, b) => a + b, 0);
+
+    this.setState(() => {
+      return {
+        cartTotal: total
       };
     });
   };
+
+  clearCart = () => {
+    this.setState(() => {
+      return { cart: [] };
+    });
+  };
+
+  removeFromCart = () => {};
 
   // increment = () => {};
   // decrement = () => {};
@@ -57,12 +88,16 @@ export default class App extends React.Component {
             <ThemeProvider
               value={{
                 ...this.state,
-                addToCart: this.addToCart
+                addToCart: this.addToCart,
+                clearCart: this.clearCart
               }}
             >
               <div className="wrapper">
                 <Navbar />
-
+                {/* conditionally render the Cart, we will have to create a
+                button which will be the cart icon to pop open the cart modal or
+                slider */}
+                {this.state.cartOpen && <Cart />}
                 <div className="container">
                   <Switch>
                     <Route exact path="/" component={Home} />
@@ -74,7 +109,6 @@ export default class App extends React.Component {
                     </Elements>
                   </Switch>
                 </div>
-
                 <Footer />
               </div>
             </ThemeProvider>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
+import { ThemeConsumer } from "../context/theme";
+import { Link } from "react-router-dom";
 
-class Cart extends Component {
-  render() {
-    <ThemeConsumer>
-      {value => {
-        return (
-          <div className="card">
-            <h1>hello</h1>
-          </div>
-        );
-      }}
-    </ThemeConsumer>;
-  }
-}
+const Cart = () => {
+  return (
+    <React.Fragment>
+      <ThemeConsumer>
+        {value => {
+          console.log(value);
+
+          if (value.cart.length > 0) {
+            return (
+              <div className="cart">
+                <h1>Cart</h1>
+                <hr />
+                {/* need to refactor this into a seperate component */}
+                {value.cart.map(item => {
+                  return (
+                    <React.Fragment>
+                      <p>Item: {item.title}</p>
+                      <p>Price: {item.price}</p>
+                      <p>---------</p>
+                    </React.Fragment>
+                  );
+                })}
+                <p>Total: {value.cartTotal}</p>
+                <hr />
+                <Link to="/checkout">
+                  <button>Checkout</button>
+                </Link>
+                <br />
+                <button
+                  onClick={() => {
+                    value.clearCart();
+                  }}
+                >
+                  Clear cart
+                </button>
+              </div>
+            );
+          } else {
+            return (
+              <div className="cart">
+                <h1>Cart</h1>
+                <h3>Cart is empty</h3>
+              </div>
+            );
+          }
+        }}
+      </ThemeConsumer>
+    </React.Fragment>
+  );
+};
 
 export default Cart;

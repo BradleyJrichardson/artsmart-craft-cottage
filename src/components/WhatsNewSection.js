ea960b3edc9d104fadc3c8d07bf1a8dbe77885aa
ms9440 <m.soheilirad@gmail.com>
2019-07-17 14:33:51 +1000

    home page whats new section

--- src/components/WhatsNewSection.js
+++ src/components/WhatsNewSection.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 import "./WhatsNewSection.css";
 import { Link } from "react-router-dom";
-import img1 from '../images/1.jpg';
-import img2 from '../images/2.jpg';
-import img3 from '../images/3.jpg';
+
+import NewProductCards from "./NewProductCards";
+import { ThemeConsumer } from "../context/theme";
 
 export default class WhatsNewSection extends Component {
   render() {
     return (
-      <section>
+      <section className='whats-new-section'>
         <h3>Whats New Section</h3>
         <div className='whatsnew'>
-          <Link to='/whatsnewdetails'>
-            <img className="img1" src={img1} alt="Generic placeholder image" />
-            <img className="img2" src={img2} alt="Generic placeholder image" />
-            <img className="img3" src={img3} alt="Generic placeholder image" />
-          </Link>
+          
+          <ThemeConsumer>
+          {value => {
+            return value.products.map((product, index) => {
+              if(product.new === true){
+                console.log(product)
+                return (
+                  <Link key={index}
+                  to={{
+                      pathname: "/productdetails",
+                      state: {
+                        product: product
+                      }
+                    }}
+                  >
+                    <NewProductCards key={index} product={product} />
+                  </Link>
+                );
+              }
+            });
+          }}
+        </ThemeConsumer>
           
         </div>
         <p>........</p>

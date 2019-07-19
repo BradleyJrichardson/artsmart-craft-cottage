c5b4a9b1903934e86776092b688a08207c7a9c9b
Cooper Viktor <im.cooperviktor@gmail.com>
2019-07-19 12:47:24 +1000

    cart layout pre lunch commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,10 +7,49 @@ const Cart = () => {
   return (
     <React.Fragment>
       <ThemeConsumer>
+      {/* <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+        </table> */}
+
         {value => {
           if (value.cart.length) {
             return (
               <div className="cart">
+                <table>
+                  <thread>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">#</th>
+                    </tr>
+                  </thread>
+                </table>
                 <h1>Cart</h1>
                 <hr />
                 {/* need to refactor this into a separate component */}
--- src/components/ContactMeForm.js
+++ src/components/ContactMeForm.js
@@ -4,7 +4,7 @@ import './ContactMeForm.css'
 export default class ContactForm extends Component {
   constructor(props) {
     super(props)
-    this.state = { fName: '', lName: '', company: '', email: '', phone: '', message: '' }
+    this.state = { name: '', company: '', email: '', phone: '', message: '' }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -19,20 +19,15 @@ export default class ContactForm extends Component {
   }
   render() {
     return (
-
        <form action='https://formspree.io/im.cooperviktor@gmail.com' method='POST' >
          <h1 className='brandTitle'>Contact Me</h1>
-
          <div className='form-group'>
-           <input placeholder='First Name:' className='form-control' type='text' value={this.state.fName} onChange={this.handleChange} name='fName' />
-           <input placeholder='Last Name:' className='form-control' type='text' value={this.state.lName} onChange={this.handleChange} name='lName' />
+            <input placeholder='Name:' className='form-control' type='text' value={this.state.name} onChange={this.handleChange} name='name' />
+            <input placeholder='Email:' className='form-control' type='text' value={this.state.email} onChange={this.handleChange} name='email' />
+            <input placeholder='Phone:' className='form-control' type='tel' value={this.state.phone} onChange={this.handleChange} name='phone' />
+            <textarea placeholder='Message:' className='form-control' type='text' value={this.state.message} onChange={this.handleChange} name='message' />
+            <input type="submit" className='form-control' value="Send Message"  />
          </div>
-         <div className='form-group'>
-           <input placeholder='Email:' className='form-control' type='text' value={this.state.email} onChange={this.handleChange} name='email' />
-           <input placeholder='Phone:' className='form-control' type='tel' value={this.state.phone} onChange={this.handleChange} name='phone' />
-         </div>
-         <textarea placeholder='Message:' className='form-control' type='text' value={this.state.message} onChange={this.handleChange} name='message' />
-         <input type="submit" className='form-control' value="Send Message"  />
        </form>
     )
   }

1f708a5349f0ccd8b17a1ca24d3a721fc4817f32
Cooper Viktor <im.cooperviktor@gmail.com>
2019-07-19 12:20:23 +1000

    contact me form bootstrapped and fixed to bottom of each page

--- src/components/ContactMeForm.js
+++ src/components/ContactMeForm.js
@@ -20,19 +20,19 @@ export default class ContactForm extends Component {
   render() {
     return (
 
-       <form className='form' action='https://formspree.io/im.cooperviktor@gmail.com' method='POST' >
+       <form action='https://formspree.io/im.cooperviktor@gmail.com' method='POST' >
          <h1 className='brandTitle'>Contact Me</h1>
 
-         <div className='namesContainer'>
-           <input placeholder='First Name:' className='textField' type='text' value={this.state.fName} onChange={this.handleChange} name='fName' />
-           <input placeholder='Last Name:' className='textField' type='text' value={this.state.lName} onChange={this.handleChange} name='lName' />
+         <div className='form-group'>
+           <input placeholder='First Name:' className='form-control' type='text' value={this.state.fName} onChange={this.handleChange} name='fName' />
+           <input placeholder='Last Name:' className='form-control' type='text' value={this.state.lName} onChange={this.handleChange} name='lName' />
          </div>
-         <div className='namesContainer'>
-           <input placeholder='Email:' className='textField' type='text' value={this.state.email} onChange={this.handleChange} name='email' />
-           <input placeholder='Phone:' className='textField' type='tel' value={this.state.phone} onChange={this.handleChange} name='phone' />
+         <div className='form-group'>
+           <input placeholder='Email:' className='form-control' type='text' value={this.state.email} onChange={this.handleChange} name='email' />
+           <input placeholder='Phone:' className='form-control' type='tel' value={this.state.phone} onChange={this.handleChange} name='phone' />
          </div>
-         <textarea placeholder='Message:' className='messageField' type='text' value={this.state.message} onChange={this.handleChange} name='message' />
-         <input type="submit" className='submitField' value="Send Message"  />
+         <textarea placeholder='Message:' className='form-control' type='text' value={this.state.message} onChange={this.handleChange} name='message' />
+         <input type="submit" className='form-control' value="Send Message"  />
        </form>
     )
   }

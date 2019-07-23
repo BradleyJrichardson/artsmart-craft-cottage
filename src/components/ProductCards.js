b51b98ea9c95ba52bb9061976326466927448169
Cooper Viktor <im.cooperviktor@gmail.com>
2019-07-24 07:50:30 +1000

    cards generated with bootstrap

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -4,26 +4,43 @@ import "./ProductCards.css";
 const ProductCards = props => {
   let { title, categories, price, images, sub_categories } = props.product;
   return (
-    <div className="category-card">
-      <div>
-        <img
-          className="image-in-whatnew-section"
-          src={images[0]}
-          alt="product"
-        />
-        <h2>{title}</h2>
+    <>
+      <div className="card">
+        <img src={images[0]} class="card-img-top" alt="product" />
+        <div className="card-body">
+          <p className="card-text">{title}</p>
+          <p className="card-text">${price}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Categories: {categories[0]}</li>
+          <li className="list-group-item">
+            Sub-Category:{" "}
+            {sub_categories[0] !== undefined && sub_categories[0] !== null
+              ? sub_categories[0]
+              : "No Sub-Category"}
+          </li>
+        </ul>
       </div>
-      <div className="card-details-spacing">
-        <p>Categories: {categories[0]}</p>
-        <p>
-          Sub-Category:{" "}
-          {sub_categories[0] !== undefined && sub_categories[0] !== null
-            ? sub_categories[0]
-            : "No Sub-Category"}
-        </p>
-        <h3>${price}</h3>
-      </div>
-    </div>
+    </>
   );
 };
 export default ProductCards;
+
+{
+  /* <div className="card" style="width: 18rem;">
+  <div>
+    <img className="image-in-whatnew-section" src={images[0]} alt="product" />
+    <h2>{title}</h2>
+  </div>
+  <div className="card-details-spacing">
+    <p>Categories: {categories[0]}</p>
+    <p>
+      Sub-Category:{" "}
+      {sub_categories[0] !== undefined && sub_categories[0] !== null
+        ? sub_categories[0]
+        : "No Sub-Category"}
+    </p>
+    <h3>${price}</h3>
+  </div>
+</div>; */
+}

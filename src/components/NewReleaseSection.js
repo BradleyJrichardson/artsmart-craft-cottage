f2122253d0578473412b43b7549e24fd014d6e0c
Cooper Viktor <im.cooperviktor@gmail.com>
2019-07-25 08:35:52 +1000

    debugging new release section

--- src/components/NewReleaseSection.js
+++ src/components/NewReleaseSection.js
@@ -5,35 +5,38 @@ import { Link } from "react-router-dom";
 import home2 from "../images/home2.jpg";
 
 export default class NewReleaseSection extends Component {
-
-  findTheProduct = (products) => {
-    return products.find((product) => {
-      return product.categories[0] === 'new release';
-    })
-  }
+  findTheProduct = products => {
+    return products.find(product => {
+      return product.categories[0] === "new release";
+    });
+  };
 
   render() {
     return (
       <ThemeConsumer>
         {value => {
-          const foundProduct = this.findTheProduct(value.products)
-          return(
-            <Link 
-            to={{
-              pathname: "/productdetails",
-              state: {
-                product: foundProduct,
-              }
-            }}
-          >
-            <section className='newRealeseSection'>
-              <h3 className="new-release-text">New Release</h3>
-              <img className="new-released-img" src={home2} alt="placeholder" />
-            </section>
-          </Link>
-          )
+          const foundProduct = this.findTheProduct(value.products);
+          return (
+            <Link
+              to={{
+                pathname: "/productdetails",
+                state: {
+                  product: foundProduct
+                }
+              }}
+            >
+              <section className="new-release-section">
+                <h3 className="new-release-text">New Release</h3>
+                <img
+                  className="new-released-img"
+                  src={home2}
+                  alt="placeholder"
+                />
+              </section>
+            </Link>
+          );
         }}
       </ThemeConsumer>
-    )
+    );
   }
 }

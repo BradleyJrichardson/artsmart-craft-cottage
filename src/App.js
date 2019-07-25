0be0ffc49aaf13d2b8f999a8b149948f66a63801
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-25 16:18:22 +1000

    env keys

--- src/App.js
+++ src/App.js
@@ -259,7 +259,7 @@ export default class App extends React.Component {
     if (this.state.products != null) {
       return (
         <Router>
-          <StripeProvider apiKey="pk_test_TlwArxmeZUHrZWhICbGMWxA100dG0fErh2">
+          <StripeProvider apiKey={process.env.REACT_APP_BACK_URL}>
             <ThemeProvider
               value={{
                 ...this.state,
